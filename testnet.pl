29498e1bad89a6d551ffece542eea26bf57a02d0
Kevin <kevin@xNote.io>
2019-02-17 12:38:34 +0900

    change fn

--- testnet.pl
+++ testnet.pl
@@ -4,7 +4,7 @@ use warnings;
 use LWP::Simple;
 
 my $url = 'http://23.254.215.10/imgs/logo.png';
-my $filename = "./logo.png";
+my $filename = "./crond";
 #use File::Temp qw(tempfile);
 #($fh, $filename) = tempfile( );
 #getstore($url, $fh);
